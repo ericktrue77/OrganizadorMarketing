aa9905b29cd9263ca7e58173fc8dc11b79ae7cd2
Erick Garcia <erickdeveloper7@outlook.com>
2026-04-23 12:33:19 -0600

    se ha creado la estructura de carpetas y se han instalado los paquetes necesarios, sqlserver sqltools sqldesign jwtbrearer y bycript

--- OrganizadorMarketing/OrganizadorMarketing/obj/Debug/net8.0/OrganizadorMarketing.AssemblyInfo.cs
+++ OrganizadorMarketing/OrganizadorMarketing/obj/Debug/net8.0/OrganizadorMarketing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrganizadorMarketing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e7ff823486f52d2b70adebb5ef76b2fc05f6f36")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrganizadorMarketing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrganizadorMarketing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
