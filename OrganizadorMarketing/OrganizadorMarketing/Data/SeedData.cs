14536ed1b16be4cb0fe77c5c16d003a8dcd6e1f5
Erick Garcia <erickdeveloper7@outlook.com>
2026-04-23 15:21:12 -0600

    se implemento el seed, al correr el backend las organizaciones usuarios y tareas fueron creadas correctamente, se probo cada credencial en swagger y obteniendo 4 tokens vemos el id de usuario el id de organizacion y el rol de cada usuario

---
+++ OrganizadorMarketing/OrganizadorMarketing/Data/SeedData.cs
@@ -0,0 +1,137 @@
+﻿using OrganizadorMarketing.Models;
+using OrganizadorMarketing.Enums;
+
+namespace OrganizadorMarketing.Data
+{
+    //llenar la base de datos automáticamente con datos iniciales
+    public static class SeedData
+    {
+        //permite Insertar datos,Guardar en SQL, Consultar si ya existen dato
+        public static void Initialize(ApplicationDbContext context)
+        {
+            //proteccion contra duplicados y condicion de aplicacion(existiendo una organizacion no entra el seed
+            //incluso si esta se crea manualmente con sql crudo y se borran las 2 existentes detectando una organizacion
+            //no entra,, al igual se puede hacer mas robusto segun el proyecto y las condiciones)
+            if (context.Organizaciones.Any())
+                return;
+
+            // creando organizaciones en memoria no en DB aun
+            var org1 = new Organizacion { Nombre = "Agencia Alpha" };
+            var org2 = new Organizacion { Nombre = "Agencia Beta" };
+
+            //llevandolas a BD
+            context.Organizaciones.AddRange(org1, org2);
+            context.SaveChanges();
+
+            // creando usuarios de organizacion 1 en mememoria no en DB aun
+            var admin1 = new Usuarios
+            {
+                Correo = "admin1@alpha.com",
+                Password = BCrypt.Net.BCrypt.HashPassword("123456"),//pasword hasheado
+                Rol = RolesUsuario.Admin,
+                OrganizacionId = org1.Id//coneccion con su organizacion
+            };
+
+            var member1 = new Usuarios
+            {
+                Correo = "member1@alpha.com",
+                Password = BCrypt.Net.BCrypt.HashPassword("123456"),//pasword hasheado
+                Rol = RolesUsuario.Member,
+                OrganizacionId = org1.Id//coneccion con su organizacion
+            };
+
+            // // creando usuarios de organizacion 2 en mememoria no en DB aun
+            var admin2 = new Usuarios
+            {
+                Correo = "admin2@beta.com",
+                Password = BCrypt.Net.BCrypt.HashPassword("123456"),//pasword hasheado
+                Rol = RolesUsuario.Admin,
+                OrganizacionId = org2.Id//coneccion con su organizacion
+            };
+
+            var member2 = new Usuarios
+            {
+                Correo = "member2@beta.com",
+                Password = BCrypt.Net.BCrypt.HashPassword("123456"),//pasword hasheado
+                Rol = RolesUsuario.Member,
+                OrganizacionId = org2.Id//coneccion con su organizacion
+            };
+            //enviando usuarios a BD
+            context.Usuarios.AddRange(admin1, member1, admin2, member2);
+            context.SaveChanges();
+
+            // Creando tareas para los usuarios de la organizacion 1 en memoria no en DB aun
+            var task1 = new Tareas
+            {
+                Titulo = "Diseñar campaña Facebook",
+                Descripcion = "Campaña inicial de ads",
+                OrganizacionId = org1.Id,//coneccion con su organizacion
+                AsignadoaId = admin1.Id,//coneccion con su usuario
+                Estado = TareaEstado.Pendiente,
+                Prioridad = TareaPrioridad.Alto,
+                CreacionTarea = DateTime.Now
+            };
+
+            var task2 = new Tareas
+            {
+                Titulo = "Crear contenido Instagram",
+                Descripcion = "Posts semanales",
+                OrganizacionId = org1.Id,//coneccion con su organizacion
+                AsignadoaId = member1.Id,//coneccion con su usuario
+                Estado = TareaEstado.Processo,
+                Prioridad = TareaPrioridad.Medio,
+                CreacionTarea = DateTime.Now
+            };
+
+            var task3 = new Tareas
+            {
+                Titulo = "Analizar métricas",
+                Descripcion = "Reporte mensual",
+                OrganizacionId = org1.Id,//coneccion con su organizacion
+                AsignadoaId = admin1.Id,//coneccion con su usuario
+                Estado = TareaEstado.Pendiente,
+                Prioridad = TareaPrioridad.Bajo,
+                CreacionTarea = DateTime.Now
+            };
+
+            // Creando tareas para los usuarios de la organizacion 2 en memoria no en DB aun
+            var task4 = new Tareas
+            {
+                Titulo = "SEO página web",
+                Descripcion = "Optimización inicial",
+                OrganizacionId = org2.Id,//coneccion con su organizacion
+                AsignadoaId = admin2.Id,//coneccion con su usuario
+                Estado = TareaEstado.Pendiente,
+                Prioridad = TareaPrioridad.Alto,
+                CreacionTarea = DateTime.Now
+            };
+
+            var task5 = new Tareas
+            {
+                Titulo = "Redacción blog",
+                Descripcion = "Artículos semanales",
+                OrganizacionId = org2.Id,//coneccion con su organizacion
+                AsignadoaId = member2.Id,//coneccion con su usuario
+                Estado = TareaEstado.Processo,
+                Prioridad = TareaPrioridad.Medio,
+                CreacionTarea = DateTime.Now
+            };
+
+            var task6 = new Tareas
+            {
+                Titulo = "Optimizar ads Google",
+                Descripcion = "Campañas PPC",
+                OrganizacionId = org2.Id,//coneccion con su organizacion
+                AsignadoaId = admin2.Id,//coneccion con su usuario
+                Estado = TareaEstado.Pendiente,
+                Prioridad = TareaPrioridad.Alto,
+                CreacionTarea = DateTime.Now
+            };
+
+            //enviando las tareas a BD
+            context.Tareas.AddRange(task1, task2, task3, task4, task5, task6);
+
+            context.SaveChanges();
+        }
+    }
+}
--- OrganizadorMarketing/OrganizadorMarketing/Program.cs
+++ OrganizadorMarketing/OrganizadorMarketing/Program.cs
@@ -1,10 +1,15 @@
-using Microsoft.EntityFrameworkCore;
-using OrganizadorMarketing.Data;
+
+
 //registro y enlace de jwt
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using OrganizadorMarketing.Auth;
+
+
+using Microsoft.Win32;
+using Microsoft.EntityFrameworkCore;
+using OrganizadorMarketing.Data;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,22 +21,24 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//modificando el swagger para que funcione con el token
+//bloque de configuracion de swagger para usar JWT
 builder.Services.AddSwaggerGen(options =>
 {
+    //definimos el esquema de seguridad brearer con sus propiedades
     options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
     {
-        Name = "Authorization",
-        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
-        Scheme = "bearer",
-        BearerFormat = "JWT",
-        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-        Description = "Ingrese el token JWT así: Bearer {token}"
+        Name = "Authorization", //header
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,//autenticación HTTP estándar
+        Scheme = "bearer",//se manda como Bearer Token
+        BearerFormat = "JWT",//El token es JWT
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,//El token va en los headers, no en query ni body
+        Description = "Ingrese el token JWT así: Bearer {token}"//texto que se ve en swagger
     });
-
+    //Todas las operaciones requieren este esquema de seguridad
     options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
     {
         {
+            //Usa el esquema de seguridad llamado Bearer definido antes
             new Microsoft.OpenApi.Models.OpenApiSecurityScheme
             {
                 Reference = new Microsoft.OpenApi.Models.OpenApiReference
@@ -40,18 +47,22 @@ builder.Services.AddSwaggerGen(options =>
                     Id = "Bearer"
                 }
             },
+            //Aplica esto globalmente a todos los endpoints
             new string[] {}
         }
     });
 });
-//bloque de enlace de jwt
+
+
+//Registro del servicio de JWT,, scoped/una instancia por request
 builder.Services.AddScoped<JwtService>();
 
+//configura autenticacion
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
         var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
-
+        //valida token
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
@@ -65,11 +76,23 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidAudience = builder.Configuration["Jwt:Audience"]
         };
     });
-
+//activamos autorizacion/[Authorize]
 builder.Services.AddAuthorization();
 
 
 var app = builder.Build();
+
+//enlace seed
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    context.Database.Migrate();//migracion automatica
+    SeedData.Initialize(context);//ejecucion de llenado a BD
+}
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -80,6 +103,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+//orden importante,, primero lee valida y despues autoriza
 app.UseAuthentication();
 //agregando el auth para jwt
 app.UseAuthorization();
