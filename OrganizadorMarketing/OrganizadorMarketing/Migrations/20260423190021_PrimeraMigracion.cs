d605a960922b90010edbcb532da25bf49cca3235
Erick Garcia <erickdeveloper7@outlook.com>
2026-04-23 13:01:43 -0600

    se realizo la primera migracion y las tablas llegaron correctamente con sus relaciones a sql server

---
+++ OrganizadorMarketing/OrganizadorMarketing/Migrations/20260423190021_PrimeraMigracion.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrganizadorMarketing.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrimeraMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organizaciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizaciones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrganizacionId = table.Column<int>(type: "int", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rol = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Usuarios_Organizaciones_OrganizacionId",
+                        column: x => x.OrganizacionId,
+                        principalTable: "Organizaciones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tareas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrganizacionId = table.Column<int>(type: "int", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AsignadoaId = table.Column<int>(type: "int", nullable: false),
+                    Estado = table.Column<int>(type: "int", nullable: false),
+                    Prioridad = table.Column<int>(type: "int", nullable: false),
+                    CreacionTarea = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LimiteTarea = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tareas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tareas_Organizaciones_OrganizacionId",
+                        column: x => x.OrganizacionId,
+                        principalTable: "Organizaciones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tareas_Usuarios_AsignadoaId",
+                        column: x => x.AsignadoaId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tareas_AsignadoaId",
+                table: "Tareas",
+                column: "AsignadoaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tareas_OrganizacionId",
+                table: "Tareas",
+                column: "OrganizacionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_OrganizacionId",
+                table: "Usuarios",
+                column: "OrganizacionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tareas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Organizaciones");
+        }
+    }
+}
--- OrganizadorMarketing/OrganizadorMarketing/obj/Debug/net8.0/OrganizadorMarketing.AssemblyInfo.cs
+++ OrganizadorMarketing/OrganizadorMarketing/obj/Debug/net8.0/OrganizadorMarketing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrganizadorMarketing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa9905b29cd9263ca7e58173fc8dc11b79ae7cd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6efca75b62aeafa9c17783ca0e46fa1e6ce7487e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrganizadorMarketing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrganizadorMarketing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
